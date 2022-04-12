58e0ea5fd928a8dd2af7801921ce65f85213bb4c
chrisjsimpson <chris.j.simpson@live.co.uk>
2022-04-12 23:27:10 +0100

    wip #6 added flask migrate and .env.example

--- src/minimalcd/__init__.py
+++ src/minimalcd/__init__.py
@@ -1,7 +1,15 @@
+from tabnanny import verbose
 from flask import Flask
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+load_dotenv(verbose=True)  # take environment variables from .env.
 
 # See https://flask.palletsprojects.com/en/2.1.x/patterns/packages/#simple-packages
-
 application = Flask(__name__)
+
+db = SQLAlchemy(application)
+migrate = Migrate(application, db)
 
 import minimalcd.views
