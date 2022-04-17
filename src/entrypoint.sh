7365c27cf82c57058b7ee674bb3f23a05a5207b5
joeltejeda <76879536+joeltejeda@users.noreply.github.com>
2022-04-17 15:41:44 -0400

    #5 adding pr preview github actions (#12)
    * #5 adding pr preview github actions
    
    * eliminating unused parameters
    
    * eliminating branch attribute
    
    * changing from review-app method to creating container
    
    * adding pr-preview create and destroy
    
    * changing the way to deploy pr preview
    
    * removing unused variables
    
    * changing default root directory
    
    * copying example.env to env file
    
    * Use dokku action for pr preview #5
    
    * #13 remove redundant deploy.yaml step
    
    * Update CHANGELOG.md [skip ci]
    
    * Revert "#13 remove redundant deploy.yaml step"
    
    This reverts commit e2942dcfb59f5024e11218ea91e9133fbd3c7553.
    
    * Revert "Revert "#13 remove redundant deploy.yaml step""
    
    This reverts commit aa9ae05667f319e4a8cdd28711c98318ce1653fb.
    
    * Update CHANGELOG.md [skip ci]
    
    * #13 keyscan for deployment
    
    * Update CHANGELOG.md [skip ci]
    
    * #13 prep ~/.ssh directory
    
    * Update CHANGELOG.md [skip ci]
    
    * #13 simplify deploy output messaging
    
    * Update CHANGELOG.md [skip ci]
    
    * #13 rm peskey : in yaml echo.
    
    * Update CHANGELOG.md [skip ci]
    
    * Update README.md
    
    * Update CHANGELOG.md [skip ci]
    
    * #5 adding pr preview github actions
    
    * eliminating unused parameters
    
    * eliminating branch attribute
    
    * changing from review-app method to creating container
    
    * adding pr-preview create and destroy
    
    * test1
    
    * changing the way to deploy pr preview
    
    * removing unused variables
    
    * changing default root directory
    
    * copying example.env to env file
    
    * Use dokku action for pr preview #5
    
    * wip #5 pr preview apps
    
    * #5 set dockerfile path
    
    * #5 set dockerfile path using preview app name
    
    * #5 set docker build path and containerfile location
    
    * #5 comment pr with url of preview url
    
    * ref #5 set preview_url as output
    
    * #5 debug environment url output

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -4,5 +4,13 @@ set -euxo pipefail
 export FLASK_APP=minimalcd
 export FLASK_DEBUG=1
 
+if [ -a .env ]
+then
+  echo ".env exists already so not copying from .env.example"
+else
+  echo ".env not found, so copying from .env.example"
+  cp .env.example .env
+fi
+
 exec uwsgi --http :80 --workers 1 --threads 2 --wsgi-file minimalcd.wsgi --touch-chain-reload minimalcd.wsgi --chdir /usr/src/app/minimalcd/
 
